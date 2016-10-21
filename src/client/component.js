2180a1b5e573d21b22668f9880e57e6972565b1b
David Ryman <David.ryman@hotmail.com>
2016-10-21 12:48:53 +0200

    Started experimenting with react

--- src/client/component.js
+++ src/client/component.js
@@ -1,11 +1,38 @@
 import React from   "react"  ;
 
 export  default React.createClass({
+	getInitialState: function () {
+		return {
+			game: {
+				state: {
+					playerInTurn: "white",
+					board: [
+						[{color: "white"}, {color:null}],
+						[{color: "black"}, {color:null}]
+					]
+				}
+			}
+		}
+	},
     render: function () {
+		console.log(this.state.game)
         return (
             <div>
-                Hello world!
+				Player in turn: {this.state.game.state.playerInTurn}
+				<button onClick={this.switchPlayer}>
+					Switch player
+				</button>
             </div>
         );
-    }
+    },
+	switchPlayer: function() {
+		var state = this.state.game.state;
+		if (state.playerInTurn === "white") {
+			state.playerInTurn = "black";
+		}
+		else {
+			state.playerInTurn = "white";
+		}
+		this.forceUpdate();
+	}
 });
